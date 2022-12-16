ff08c0becd516af3eea93c6a2426b4e34bbebdc1
mudouasenha <mudouasenha@gmail.com>
2022-12-16 02:32:54 -0300

    Novo app

---
+++ aspnetcore-l20n-i18n/aspnetcore-l20n-i18n.Api/Models/Movie.cs
@@ -0,0 +1,26 @@
+#define First
+#if First
+
+#region snippet1
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
+
+#endregion snippet1
+
+#endif
